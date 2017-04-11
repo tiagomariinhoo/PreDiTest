264d5321c8095c85e6470f02b6809c4dbdfd179b
wagnerfilho1995 <wagnerfilho1995@gmail.com>
2017-04-10 22:55:27 -0300

    - Pos login

--- app/src/main/java/app/com/example/wagner/meupredi/PosLogin.java
+++ app/src/main/java/app/com/example/wagner/meupredi/PosLogin.java
@@ -1,18 +1,68 @@
 package app.com.example.wagner.meupredi;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Pichau on 08/04/2017.
  */
 
 public class PosLogin extends AppCompatActivity {
+
+    private TextView nomeUsuario;
+    private EditText idade;
+    private EditText peso;
+    private EditText circunferecia;
+    private Button cancelar;
+    private Button concluir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poslogin);
+
+        nomeUsuario = (TextView) findViewById(R.id.text_nome_usuario_poslogin);
+        idade = (EditText) findViewById(R.id.edit_idade_poslogin);
+        peso = (EditText) findViewById(R.id.edit_peso_poslogin);
+        circunferecia = (EditText) findViewById(R.id.edit_circunferencia_poslogin);
+        cancelar = (Button) findViewById(R.id.btn_cancelar_poslogin);
+        concluir = (Button) findViewById(R.id.btn_concluir_poslogin);
+
+        findViewById(R.id.tela_pos_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getCurrentFocus()!=null && getCurrentFocus() instanceof EditText){
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(idade.getWindowToken(), 0);
+                    imm.hideSoftInputFromWindow(peso.getWindowToken(), 0);
+                    imm.hideSoftInputFromWindow(circunferecia.getWindowToken(), 0);
+                }
+            }
+        });
+
+        concluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menuPrincipal = new Intent(PosLogin.this, MenuPrincipal.class);
+                startActivity(menuPrincipal);
+            }
+        });
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent voltaLogin = new Intent(PosLogin.this, TelaLogin.class);
+                startActivity(voltaLogin);
+            }
+        });
 
     }
 }
