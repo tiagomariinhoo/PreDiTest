c3b4177a9d350d758851ba1e9ab7f05ac82c506a
Wagner Williams <wagnerfilho1995@gmail.com>
2017-07-14 11:01:09 -0300

    Cronometro, finalizar treino e ainfs

--- app/src/main/java/app/com/example/wagner/meupredi/Exercicios.java
+++ app/src/main/java/app/com/example/wagner/meupredi/Exercicios.java
@@ -1,10 +1,12 @@
 package app.com.example.wagner.meupredi;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,9 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 import app.com.example.wagner.meupredi.BDMenuLogin.Paciente;
 
@@ -22,11 +27,13 @@ import app.com.example.wagner.meupredi.BDMenuLogin.Paciente;
 
 public class Exercicios extends Fragment {
 
-    private TextView chamadaDesempenho;
+    private ImageView chamadaDesempenho;
     private ImageView chamadaGinasio;
-    private Button start, reset, pause;
+    private ImageView start;
+    private Button reset, pause;
     private Chronometer cronometro;
     private long ultimaPausa;
+    private TextView finalizar;
     private Paciente paciente;
 
     @Nullable
@@ -35,11 +42,11 @@ public class Exercicios extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_exercicios, container, false);
 
-        start = (Button) view.findViewById(R.id.btn_start_exercicios);
+        start = (ImageView) view.findViewById(R.id.btn_start_exercicios);
         pause = (Button) view.findViewById(R.id.btn_pause_exercicios);
         reset = (Button) view.findViewById(R.id.btn_reset_exercicios);
         cronometro = (Chronometer) view.findViewById(R.id.crn_cronometro_exercicio);
-
+        finalizar = (TextView) view.findViewById(R.id.text_finalizar_treino);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +63,6 @@ public class Exercicios extends Fragment {
             }
         });
 
-
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,7 +73,6 @@ public class Exercicios extends Fragment {
             }
         });
 
-
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,6 +81,61 @@ public class Exercicios extends Fragment {
                 ultimaPausa = 0;
                 pause.setEnabled(false);
                 start.setEnabled(true);
+            }
+        });
+
+        finalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //CODIGO PARA PAUSAR O CRONOMETRO
+                ultimaPausa = SystemClock.elapsedRealtime();
+                cronometro.stop();
+                pause.setEnabled(false);
+                start.setEnabled(true);
+
+                new AlertDialog.Builder(getActivity()).setTitle("Finalizar Treino")
+                        .setMessage("Deseja realmente finalizar o seu treino?")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // CODIGO PARA RESETAR O CRONOMETRO
+                                cronometro.stop();
+                                cronometro.setBase(SystemClock.elapsedRealtime());
+                                ultimaPausa = 0;
+                                pause.setEnabled(false);
+                                start.setEnabled(true);
+                                Toast.makeText(getActivity(), "Xablau", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // CODIGO PARA VOLTAR A RODAR O CRONOMETRO
+
+                                    if(ultimaPausa != 0) {
+                                        cronometro.setBase(cronometro.getBase() + SystemClock.elapsedRealtime() - ultimaPausa);
+                                    }
+
+                                    else{
+                                        cronometro.setBase(SystemClock.elapsedRealtime());
+                                    }
+                                    cronometro.start();
+                                    start.setEnabled(false);
+                                    pause.setEnabled(true);
+
+                            }
+                        }).show();
+
+                // PEGANDO O PROGRESSO DO PACIENTE AO FINALIZAR O TREINO
+                long timeElapsed = ultimaPausa - cronometro.getBase();
+                int hours = (int) (timeElapsed / 3600000);
+                int minutes = (int) (timeElapsed - hours * 3600000) / 60000;
+                int seconds = (int) (timeElapsed - hours * 3600000 - minutes * 60000) / 1000;
+
+                Log.d("hora: ",String.valueOf(hours));
+                Log.d("minutes: ",String.valueOf(minutes));
+                Log.d("seconds: ",String.valueOf(seconds));
             }
         });
 
@@ -91,7 +151,7 @@ public class Exercicios extends Fragment {
             }
         });
 
-        chamadaDesempenho = (TextView) view.findViewById(R.id.text_veja_seu_desempenho);
+        chamadaDesempenho = (ImageView) view.findViewById(R.id.text_veja_seu_desempenho);
         chamadaDesempenho.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,4 +169,5 @@ public class Exercicios extends Fragment {
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Exercicios");
     }
+
 }
